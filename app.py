1bd08b8fe224b4334ce3947286d5b94265ae226e
lchrimes <lchrimes@databound.co.uk>
2021-04-12 09:57:52 +0100

    updated logic within all functions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import itertools as it
+import math
 
 # ------------------ Tab Title & Header ------------------ #
 
@@ -40,27 +41,67 @@ st.markdown(intro_streamlit_style, unsafe_allow_html=True)
 # ------------------ Application Functions ------------------ #
 
 
+def group_size_recommender(attendees_num):
+
+    if attendees_num <= 12:
+        group_size_max = 2
+    if attendees_num >= 18:
+        group_size_max = 4
+    if attendees_num >= 30:
+        group_size_max = 6
+    if attendees_num >= 50:
+        group_size_max = 8
+    if attendees_num % 2 != 0:
+        group_size_max += 1
+
+    return group_size_max
+
+
+def session_recommender(group_size: int, attendees: list):
+
+    chunked_data = generate_groups(group_size, attendees)
+
+    needed_sessions = list(it.combinations(chunked_data, 2))
+
+    session_num = len(needed_sessions) / 2 - 1
+    if session_num <= 0:
+        session_num = 1
+
+    return int(session_num)
+
+
 def generate_groups(group_size: int, attendees: list):
 
     chunked_data = [
         attendees[i : i + group_size] for i in range(0, len(attendees), group_size)
     ]
+    # print(chunked_data)
+    if len(chunked_data[-1]) == 1:
+        lone = chunked_data.pop()
+        chunked_data[-1] += lone
+
+    if len(chunked_data) % 2 != 0:
+        last_group = chunked_data.pop()
+        while last_group:
+            for group in chunked_data:
+                while last_group:
+                    name = last_group.pop()
+                    group.append(name)
+                    break
+                continue
 
     return chunked_data
 
 
 def generate_sesisons(sessions: int, group_size: int, attendees: list):
 
-    chunked_data = [
-        attendees[i : i + group_size] for i in range(0, len(attendees), group_size)
-    ]
+    chunked_data = generate_groups(group_size, attendees)
 
     rooms = len(chunked_data) / 2
     needed_sessions = list(it.combinations(chunked_data, 2))
     sorted_sessions = {}
 
     for session in range(sessions):
-        print(f"Session {session}")
         sorted_sessions[session] = {}
         picked = []
         for room in range(int(rooms)):
@@ -73,42 +114,11 @@ def generate_sesisons(sessions: int, group_size: int, attendees: list):
                     if name in set(picked):
                         next_sesion = True
                 if not next_sesion:
-                    print(f" {room} {pick_session}")
                     picked += flatted_session
                     sorted_sessions[session][room] = flatted_session
                     needed_sessions.pop(i)
                     break
     return sorted_sessions
-
-
-def session_recommender(group_size: int, attendees: list):
-
-    chunked_data = [
-        attendees[i : i + group_size] for i in range(0, len(attendees), group_size)
-    ]
-
-    return len(chunked_data) - 1
-
-
-def group_size_recommender(attendees_num, min_group_size=2):
-
-    if attendees_num / min_group_size >= 1:
-        group_size_max = 2
-    if attendees_num / min_group_size >= 8:
-        group_size_max = 4
-    if attendees_num / min_group_size >= 14:
-        group_size_max = 6
-    if attendees_num / min_group_size >= 24:
-        group_size_max = 8
-    if attendees_num / min_group_size >= 30:
-        group_size_max = 12
-    if attendees_num / min_group_size >= 40:
-        group_size_max = 16
-
-    if attendees_num % 2 != 0:
-        group_size_max += 1
-
-    return group_size_max
 
 
 # ------------------ User Input ------------------ #
@@ -128,7 +138,7 @@ if attendees != "" and len(attendees) > 3:
     rec_session_num = session_recommender(rec_group_size, attendees)
 
     responsive_attendees_num = f"You have {attendees_num} attendees"
-    responsive_rec_group_size = f"Based on the number of attendees, we recommend a max group size of {rec_group_size}"
+    responsive_rec_group_size = f"Based on the number of attendees, we recommend a min group size of {rec_group_size}"
     responsive_rec_session = f"Based on this group size, you would require {rec_session_num} session for everyone to meet"
 
     st.text(responsive_attendees_num)
@@ -154,8 +164,6 @@ if trigger:
 
     original_groups = generate_groups(group_size, attendees)
     planned_sessions: dict = generate_sesisons(num_sessions, group_size, attendees)
-    # st.text(original_groups)
-    # st.text(planned_sessions)
 
     cols = st.beta_columns(num_sessions + 1)
     cols[0].write("<h4><u>Original Groups</u></h4>", unsafe_allow_html=True)
