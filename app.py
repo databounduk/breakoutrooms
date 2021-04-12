368a9850a216a4513fbed0aa32fa89aba251201c
lchrimes <lchrimes@databound.co.uk>
2021-04-12 12:02:10 +0100

    updates

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 st.set_page_config(
     page_title="Breakout Rooms", initial_sidebar_state="auto",
 )
-st.title("Breakout Rooms")
+st.title("Breakout Rooms - Prototype")
 
 # ------------------ Markdowns ------------------ #
 
@@ -20,21 +20,22 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 intro_streamlit_style = """
-            <p>
+            <h4>Requirements and Recommendations:</h4>
+            <li>
             Please provide at least 2 names/attendees.
-            </p>
-            <p>
+            </li>
+            <li>
             Each name needs to be on a new line, copy and paste a col from a spread sheet.
-            </p>
-            <p>
+            </li>
+            <li>
             Names must be unique.
-            </p>
-            <p>
+            </li>
+            <li>
             We do not recommend using this system recomendations for parties over 60.
-            </p>
-            <p>
-            A csv will be avaiable for the pre-assigned groups that relate to the starting groups.
-            </p>
+            </li>
+            <li>
+            We suggest pressing on generate a few times to get the optimal arrangement
+            </li>
             """
 st.markdown(intro_streamlit_style, unsafe_allow_html=True)
 
@@ -43,14 +44,13 @@ st.markdown(intro_streamlit_style, unsafe_allow_html=True)
 
 def group_size_recommender(attendees_num):
 
-    if attendees_num <= 12:
-        group_size_max = 2
+    group_size_max = 2
+    if attendees_num >= 12:
+        group_size_max = 3
     if attendees_num >= 20:
         group_size_max = 4
     if attendees_num >= 30:
         group_size_max = 6
-    if attendees_num >= 40:
-        group_size_max = 8
 
     return group_size_max
 
@@ -116,7 +116,9 @@ def generate_sesisons(sessions: int, group_size: int, attendees: list):
                     sorted_sessions[session][room] = flatted_session
                     needed_sessions.pop(i)
                     break
-    return sorted_sessions
+    filter_sessions = {i: session for i, session in sorted_sessions.items() if session}
+    print(filter_sessions)
+    return filter_sessions
 
 
 # ------------------ User Input ------------------ #
@@ -131,18 +133,21 @@ attendees = [
 # ------------------ Responsive Input ------------------ #
 
 if attendees != "" and len(attendees) > 3:
+
+    # Constant Variable Setting
     attendees_num = len(attendees)
     rec_group_size = group_size_recommender(attendees_num)
     rec_session_num = session_recommender(rec_group_size, attendees)
 
+    # On Screen Responses
     responsive_attendees_num = f"You have {attendees_num} attendees"
     responsive_rec_group_size = f"Based on the number of attendees, we recommend a min group size of {rec_group_size}"
     responsive_rec_session = f"Based on this group size, you would require {rec_session_num} session for everyone to meet"
-
     st.text(responsive_attendees_num)
     st.text(responsive_rec_group_size)
     st.text(responsive_rec_session)
 
+    # Setting the slider value
     group_size = st.slider(
         "Group Size", min_value=2, max_value=attendees_num, value=rec_group_size
     )
